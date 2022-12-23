ce2197aa1103eb5a3b3aeed6dace00b927214428
tijana <tijana822@gmail.com>
2022-12-22 21:27:34 -0500

    test: qa-98 add testing subscr.

---
+++ src/tijana/TestingSubscription.java
@@ -0,0 +1,10 @@
+package tijana;
+
+public class TestingSubscription {
+
+    public static void main(String[] args) {
+
+        System.out.println("I am testing campaign for subscription func");
+    }
+
+}
